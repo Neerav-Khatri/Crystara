cf6e38b6689c38470265f100deaee8bae99aa9e3
Mustaqhasan <shaikhmustaq7714@gmail.com>
2023-02-24 13:35:54 +0530

    Changed domain in config.js

--- crystara/next.config.js
+++ crystara/next.config.js
@@ -2,16 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-<<<<<<< HEAD
-    domains: [`countryflagsapi.com`, `banner.caratlane.com`,"cdn.caratlane.com"],
-=======
     domains: [
       `countryflagsapi.com`,
       `banner.caratlane.com`,
+      "cdn.caratlane.com",
       `web-assets.payu.in`,
     ],
->>>>>>> f151dbdcc7e8daec25f7b3126079a4f149445591
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
